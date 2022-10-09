acda27393decad19dddc17403b4799bc9ef8e73e
Yoshihiro Fujita <ysfuji3sge@gmail.com>
2022-10-09 13:58:34 +0900

    Add Create Image proc

--- samples/pubsub.py
+++ samples/pubsub.py
@@ -10,6 +10,8 @@ import json
 import asyncio
 import time
 import RPi.GPIO as GPIO
+import cv2
+import threading
 
 # This sample uses the Message Broker for AWS IoT to send and receive messages
 # through an MQTT connection. On startup, the device connects to the server,
@@ -32,8 +34,9 @@ cmdUtils.register_command("count", "<int>", "The number of messages to send (opt
 # Needs to be called so the command utils parse the commands
 cmdUtils.get_args()
 
-received_count = 0
-received_all_event = threading.Event()
+chime_message_topic = 'chime/message'
+image_request_topic = 'image/request'
+image_release_topic = 'image/release'
 
 GPIO.setmode(GPIO.BCM)
 
@@ -42,6 +45,9 @@ GPIO.setup(15, GPIO.OUT)
 GPIO.output(15, GPIO.LOW)
 
 queue = asyncio.Queue(maxsize=2)
+queue_message_chime = "chime_{}"
+
+threadEvent = threading.Event()
 
 # Callback when connection is accidentally lost.
 def on_connection_interrupted(connection, error, **kwargs):
@@ -70,13 +76,15 @@ def on_resubscribe_complete(resubscribe_future):
                 sys.exit("Server rejected resubscribe to topic: {}".format(topic))
 
 
+async def queue_image_task_put(str):
+    await queue_image_task.put(str)
+
 # Callback when the subscribed topic receives a message
 def on_message_received(topic, payload, dup, qos, retain, **kwargs):
     print("Received message from topic '{}': {}".format(topic, payload))
-    global received_count
-    received_count += 1
-    if received_count == cmdUtils.get_command("count"):
-        received_all_event.set()
+    if topic == image_request_topic :
+        print("image create Req")
+        threadEvent.set()
 
 #
 async def task_aws_iotcore_main_proc():
@@ -91,20 +99,16 @@ async def task_aws_iotcore_main_proc():
     connect_future.result()
     print("Connected!")
 
-    message_count = cmdUtils.get_command("count")
-    # message_topic = cmdUtils.get_command(cmdUtils.m_cmd_topic)
-    message_topic = 'chime/message'
-    # message_string = cmdUtils.get_command(cmdUtils.m_cmd_message)
 
     # Subscribe
-    # print("Subscribing to topic '{}'...".format(message_topic))
-    # subscribe_future, packet_id = mqtt_connection.subscribe(
-    #     topic=message_topic,
-    #     qos=mqtt.QoS.AT_LEAST_ONCE,
-    #     callback=on_message_received)
+    print("Subscribing to topic '{}'...".format(image_request_topic))
+    subscribe_future, packet_id = mqtt_connection.subscribe(
+        topic=image_request_topic,
+        qos=mqtt.QoS.AT_LEAST_ONCE,
+        callback=on_message_received)
 
-    # subscribe_result = subscribe_future.result()
-    # print("Subscribed with {}".format(str(subscribe_result['qos'])))
+    subscribe_result = subscribe_future.result()
+    print("Subscribed with {}".format(str(subscribe_result['qos'])))
 
     # Publish message to server desired number of times.
     # This step is skipped if message is blank.
@@ -113,29 +117,26 @@ async def task_aws_iotcore_main_proc():
         if queue.qsize():
             print("queue.get()")
             qdata = await queue.get()
+            print(qdata)
         else:
             await asyncio.sleep(0)
             continue
-        # message = "{} [{}]".format(message_string, publish_count)
-        message = {'chime': 'ON'}
-        message['chime'] = qdata
-        print("Publishing message to topic '{}': {}".format(message_topic, message))
-        message_json = json.dumps(message)
-        print(message_json)
-        mqtt_connection.publish(
-            topic=message_topic,
-            payload=message_json,
-            qos=mqtt.QoS.AT_LEAST_ONCE)
+        q_message = qdata.split('_')
+        if q_message[0] == 'chime' :
+            message = {'chime': 'ON'}
+            message['chime'] = q_message[1]
+            print("Publishing message to topic '{}': {}".format(chime_message_topic, message))
+            message_json = json.dumps(message)
+            print(message_json)
+            mqtt_connection.publish(
+                topic=chime_message_topic,
+                payload=message_json,
+                qos=mqtt.QoS.AT_LEAST_ONCE)
+        elif q_message[0] == 'image' :
+            await queue_image_task.put('create')
+            
         #time.sleep(1)
         #await asyncio.sleep(1)
-
-    # Wait for all messages to be received.
-    # This waits forever if count was set to 0.
-    if message_count != 0 and not received_all_event.is_set():
-        print("Waiting for all messages to be received...")
-
-    received_all_event.wait()
-    print("{} message(s) received.".format(received_count))
 
     # Disconnect
     print("Disconnecting...")
@@ -154,24 +155,54 @@ async def task_io_main_proc():
             if pinstate == 1: # Chime ON
                 if preStatus != 1:
                     print("Chime ON")
-                    await queue.put('ON');
+                    await queue.put(queue_message_chime.format('ON'));
                     preStatus = 1
                     GPIO.output(15, GPIO.HIGH)
             else: # Chime OFF
                 if preStatus != 0:
                     preStatus = 0
                     print("Chime OFF")
-                    await queue.put('OFF');
+                    await queue.put(queue_message_chime.format('OFF'));
                     GPIO.output(15, GPIO.LOW)
             await asyncio.sleep(0)
     finally:
         GPIO.output(15, GPIO.LOW)
 
+async def task_image_create_proc():
+    print("task_image_create_proc START")
+
+    user_id = "6199"
+    user_pw = "4003"
+    host = "10.108.34.17"
+
+    try:
+        while(True):
+            if threadEvent.wait(0) == True :
+                threadEvent.clear()
+                print("Image Create Req Received!")
+                cap = cv2.VideoCapture(f"rtsp://{user_id}:{user_pw}@{host}/live")
+                ret, frame = cap.read()
+                if ret == True:
+                    # cv2.imshow('VIDEO', frame)
+                    cv2.imwrite('./test.jpg', frame)
+                cv2.waitKey(1)
+                cap.release()
+                cv2.destroyAllWindows()
+            else:
+                await asyncio.sleep(0)
+    except Exception as e:
+        print("Exception Occured: {}".format(e))
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
+
 async def create_task():
     task1 = asyncio.create_task(task_io_main_proc())
     task2 = asyncio.create_task(task_aws_iotcore_main_proc())
+    task3 = asyncio.create_task(task_image_create_proc())
     await task1
     await task2
+    await task3
 
 if __name__ == '__main__':
 
